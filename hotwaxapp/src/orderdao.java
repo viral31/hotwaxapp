cf8d67c1add06bc1fb63d527e75f67c2a5bfa6a8
Viral Maru <viralmaru571@gmail.com>
2023-12-30 20:22:23 +0530

    .

--- hotwaxapp/src/orderdao.java
+++ hotwaxapp/src/orderdao.java
@@ -15,19 +15,23 @@ public class orderdao {
         String query = "insert into Order_Header values (?,?,?,?,?,?,?,?,?,?)";
         try {
             PreparedStatement pstm = this.con.prepareStatement(query);
-            pstm.setString(1,null);
+            pstm.setString(1,"1222");
             pstm.setString(2,o.getOrderName());
             pstm.setString(6,o.getCurrencyUomId());
             pstm.setString(8,o.getSalesChannelEnumId());
             pstm.setString(5,o.getStatusId());
             pstm.setString(7,o.getProductStoreId());
-            pstm.setDate(3, (Date) o.getPlacedDate());
-            pstm.setDate(4, (Date) o.getApprovedDate());
+            java.sql.Date sqlDate1 = new java.sql.Date(o.getPlacedDate().getTime());
+
+            pstm.setDate(3, sqlDate1);
+            java.sql.Date sqlDate = new java.sql.Date(o.getApprovedDate().getYear());
+
+            pstm.setDate(4,sqlDate);
             pstm.setDouble(9,1000);
             pstm.setDate(10,null);
             int cnt = pstm.executeUpdate();
             if (cnt != 0) {
-                System.out.println("person inserted successfully");
+                System.out.println("ORDER inserted successfully");
             }
 
         }catch (Exception var5) {
